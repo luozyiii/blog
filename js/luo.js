dd24814385c1978e9c73e7f47ba501768bebbce2
罗智毅 <luozhiyi@luozhiyideMacBook-Pro.local>
2016-03-24 16:39:56 +0800

    导航

--- js/luo.js
+++ js/luo.js
@@ -3,9 +3,16 @@ var LUO = {
     nav:function () {
         var _url = window.location.href;
         if (_url.indexOf("web") >= 0) {
-            _nav.removeClass("active");
             $(".classification li:eq(2)").addClass("active");
-        } 
+        } else if(_url.indexOf("ui") >= 0) {
+            $(".classification li:eq(3)").addClass("active");
+        } else if(_url.indexOf("other") >= 0) {
+            $(".classification li:eq(3)").addClass("active");
+        } else if(_url.indexOf("about") >= 0) {
+            $(".classification li:eq(4)").addClass("active");
+        } else {
+            $(".classification li:eq(1)").addClass("active");
+        }
     },
 	//返回顶部
     go_top:function(){
